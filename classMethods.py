384f7793b568a7128cea8fc953b89b09d312615f
Ethan H <ethanholst4@gmail.com>
2022-05-22 17:58:46 +1000

    Signup and Login Functions
    Creates functions for the signup and login for users. Just a placeholder to manage accounts so is unencrypted and stores the details in text in an SQL file. Can read and write into a file, scanning through existing users and checking if the accounts already exists and if the details are matching.

--- classMethods.py
+++ classMethods.py
@@ -1,23 +1,23 @@
 import sqlite3
 import os
-import Main
 
 path = os.path.dirname(os.path.abspath(__file__))
-db = os.path.join(path, 'QuestionDatabase.db')
+DBpath = os.path.join(path, 'QuestionDatabase.db')
 
 def addFollowup(Text, Cat):
-    conn = sqlite3.connect(db)
+    conn = sqlite3.connect(DBpath)
     cursor = conn.cursor() 
     cursor.execute('SELECT COUNT(*) from FollowupQuestions')
     cursorResult = cursor.fetchone()
     QuesID = cursorResult[0] + 1
 
-    cursor.execute("insert into FollowupQuestions (QuestionID, QuestionText, Category) values (?, ?, ?)", (QuesID, Text, Cat))
+    cursor.execute("INSERT INTO FollowupQuestions (QuestionID, QuestionText, Category) VALUES (?, ?, ?)", (QuesID, Text, Cat))
     conn.commit()
     cursor.close()
 
+# unfinished (not working)
 def removeFollowup(QuestionID):
-    conn = sqlite3.connect(db)
+    conn = sqlite3.connect(DBpath)
     cursor = conn.cursor() 
 
     code = ('DELETE FROM FollowupQuestions WHERE QuestionID = {id}').format(id = QuestionID)
@@ -25,4 +25,44 @@ def removeFollowup(QuestionID):
 
     cursor.close()
 
-removeFollowup(10)
+def login(username, password):
+    connect = sqlite3.connect(DBpath)
+    cursor = connect.cursor() 
+
+    cursor.execute('SELECT Username, Password from Accounts')
+    accounts = cursor.fetchall()
+    cursor.close()
+
+    accountFound = False
+    i = 0
+    while accountFound == False and i < len(accounts):
+        if (accounts[i][0].lower() == username.lower() and accounts[i][1] == password):
+            print("stub: log in successsful")
+            accountFound = True
+            break
+        i = i + 1
+    if accountFound == False:
+            print("stub: account does not exist or password is incorrect")
+
+def signup(email, username, password):
+    connect = sqlite3.connect(DBpath)
+    cursor = connect.cursor() 
+    cursor.execute('SELECT Email, Username, Password from Accounts')
+    accounts = cursor.fetchall()
+    cursor.close()
+
+    accountFound = False
+    i = 0
+    while accountFound == False and i < len(accounts):
+        if (accounts[i][0].lower() == email.lower() or accounts[i][1].lower() == username.lower()):
+            print("stub: account username or password already exists")
+            accountFound = True
+            break
+        i = i + 1   
+    
+    if (accountFound == False):
+        connect = sqlite3.connect(DBpath)
+        cursor = connect.cursor() 
+        cursor.execute("INSERT INTO Accounts (Email, Username, Password, AccountType) values (?, ?, ?)", (email, username, password))
+        cursor.close()
+
